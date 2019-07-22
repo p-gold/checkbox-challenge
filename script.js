5b2127b946dd5567934c54cfb9e211a86859ad55
pgold <ochukwudi205@gmail.com>
2019-07-22 23:23:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// THIS PROJECT INSULTED ME A LOT
+checkboxes = document.querySelectorAll('.item input[type = "checkbox"]');
+let lastChecked;
+function handleCheck(e) {
+ let inBetween = false;
+ if (e.shiftKey && lastChecked.checked && this.checked && lastChecked != this) {
+  checkboxes.forEach(checkbox => {
+   if (checkbox === this || checkbox === lastChecked) {
+    inBetween = !inBetween;
+   }
+   inBetween ? (checkbox.checked = true) : "";
+  });
+ }
+ lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => {
+ checkbox.addEventListener("click", handleCheck);
+});
